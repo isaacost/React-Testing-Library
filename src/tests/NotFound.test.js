a4921aade8b2f5ee771019f594fea1816a17859f
Isa Costa <isa-hcosta@hotmail.com>
2022-09-19 16:47:59 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  it(
+    'Verifica se a página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const titleNotFound = screen.getByRole(
+        'heading',
+        { name: 'Page requested not found' },
+      );
+      expect(titleNotFound).toBeInTheDocument();
+    },
+  );
+  it('Verifica se a pagina mostra uma determinada imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(imgNotFound).toBeInTheDocument();
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
